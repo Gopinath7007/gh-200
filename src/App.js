5c7cb375952e2c1b5d8d3fc897fd2619ade0e0f3
Gopinath K <gopinathkaliappan@gmail.com>
2025-11-28 19:01:59 +0530

    boss new

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function App() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <div style={{ textAlign: 'cengitter', marginTop: '50px' }}>
       <h1>Hello from React!</h1>
-      <p>Welcome to gh-200 React App boss</p>
+      <p>Welcome to gh-200 React App boss new</p>
     </div>
   );
 }
