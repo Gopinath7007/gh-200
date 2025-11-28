dcaec0915a100a6b74a279d9a25fbf28e65d1001
Gopinath K <gopinathkaliappan@gmail.com>
2025-11-28 17:48:25 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello from React!</h1>
+      <p>Welcome to gh-200 React App</p>
+    </div>
+  );
+}
+
+export default App;
