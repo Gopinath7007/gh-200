b317bb2113ad03b2d38a61da71b0b515e6df8970
Gopinath K <gopinathkaliappan@gmail.com>
2025-11-28 18:59:01 +0530

    check changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Hello from React!</h1>
-      <p>Welcome to gh-200 React App</p>
+      <p>Welcome to gh-200 React App boss</p>
     </div>
   );
 }
