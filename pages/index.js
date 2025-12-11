aaef95a35a30b0f943aca80b947e607c6b359378
Gopinath K <gopinathkaliappan@gmail.com>
2025-12-11 14:57:24 +0530

    deploy changes

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{textAlign: 'center', marginTop: 40}}>
+      <h1>Welcome to Next.js Home!</h1>
+      <p>This is the Home page.</p>
+    </main>
+  );
+}
---
+++ pages/privacy.js
@@ -0,0 +1,8 @@
+export default function Privacy() {
+  return (
+    <main style={{textAlign: 'center', marginTop: 40}}>
+      <h1>Privacy Policy</h1>
+      <p>This is the Privacy page.</p>
+    </main>
+  );
+}
